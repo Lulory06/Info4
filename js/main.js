4359f6960056bcc9e656de9f21ee39eb5afbf837
unknown <lorenzo.lupi1@iisghisleri.local>
2024-04-03 10:40:43 +0200

    Decrementa Js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function calcolaFattorialeIncrementa() {
         risultato = risultato * i;
   }
 
-  document.getElementById("risFattoriale").textContent = "Il Risulatato è " + risultato;
+  document.getElementById("risFattoriale").textContent = "Il fattoriale di " + n + " è " + risultato;
 }
 
 function sommaNNumPrimi(){
@@ -23,4 +23,24 @@ function sommaNNumPrimi(){
 function calcolaQuadrato(){
     var num = parseInt(document.getElementById("inputNumber").value);
     document.getElementById("risQuadrato").textContent = "Il quadrato di " + num + " è " + num*num
+}
+
+function calcolaFattorialeDecrementa(){
+    var n = parseInt(document.getElementById("inputNumber").value);
+    var risultato = 1;
+
+    for (var i = n; i >= 1; i--){
+        risultato = risultato * i;
+    }
+    document.getElementById("risFattoriale").textContent = "Il fattoriale di " + n + " è " + risultato;
+}
+
+function calcolaSommaNNumDecrementa(){
+    var num = parseInt(document.getElementById("inputNumber").value);
+    var somma = 0;
+
+    for (var i = num; i >= 1; i--){
+        somma = somma + i; 
+    }
+    document.getElementById("risSommaNum").textContent = "La somma dei primi " + num + " numeri è " + somma;
 }
